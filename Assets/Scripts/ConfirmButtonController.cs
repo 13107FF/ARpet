f79d27d240c3f103729f24bebd0a75c6ac78700c
13107FF <3170102473@zju.edu.cn>
2020-06-25 11:40:36 +0800

    add pet selection page

---
+++ Assets/Scripts/ConfirmButtonController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConfirmButtonController : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("HelloAR");
+        Debug.Log("I was clicked!");
+    }
+}
---
+++ Assets/Scripts/PetSelection.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PetSelection : MonoBehaviour
+{
+    public GameObject[] petPrefabs; //用于存放宠物数组
+    private int selectedIndex = 0; //创建选择索引的变量
+    // Start is called before the first frame update
+    void Start()
+    {
+        petShow();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void petShow()
+    {
+        petPrefabs[selectedIndex].SetActive(true);
+        Debug.Log("PetShow: "+selectedIndex.ToString());
+        for (int i = 0; i < petPrefabs.Length; i++)
+            if (i != selectedIndex)
+                petPrefabs[i].SetActive(false);
+    }
+
+
+
+    public void onLeftButtonClick()
+    {
+        selectedIndex--;
+        if (selectedIndex == -1)
+            selectedIndex = petPrefabs.Length - 1;
+        petShow();
+        Debug.Log("Left was clicked!");
+    }
+
+    public void onRightButtonClick()
+    {
+        selectedIndex++;
+        selectedIndex %= petPrefabs.Length;
+        petShow();
+        Debug.Log("Right was clicked!");
+    }
+}
